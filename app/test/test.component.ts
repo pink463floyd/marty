db6f78a2b98b304a0017a23924c142015faee04f
Scott Larribeau <slarribeau@gmail.com>
2018-04-26 15:26:57 -0700

    added columns to standings

--- app/test/test.component.ts
+++ app/test/test.component.ts
@@ -3,6 +3,7 @@ import {MlbRepoService} from '../mlb-repo.service';
 //https://www.dropbox.com/sh/0sankm6febsfogp/AAB1p-aVJnoReeclPZdv6jYua/Raw%20Icons%20-%20AL?dl=0
 //http://resizeimage.net/
 
+
 @Component({
   selector: 'app-test',
   template: `
@@ -10,16 +11,22 @@ import {MlbRepoService} from '../mlb-repo.service';
       <tr> 
         <th></th>
         <th>Team</th>
-        <th>W</th>
-        <th>L</th>
+        <th style="text-align:right">W</th>
+        <th style="text-align:right">L</th>
+        <th>PCT</th>
+        <th>GB</th>
+
       </tr>
       <tr *ngFor="let standing of standings">
          <td> 
           <img src="../assets/icon.png" alt="some text" width=16 height=16 style="float:left;">
          </td>
-         <td style="width:30px;  ">{{standing.Team}}  </td>
-        <td style="width:100px; ">{{standing.W}} </td>
-        <td style="width:50px;  ">{{standing.L}}</td>
+         <td style="width:80px;  ">{{standing.Team}}  </td>
+        <td style="width:150px; ">{{standing.W}} </td>
+        <td style="width:100px;  ">{{standing.L}}</td>
+        <td style="width:200px;  ">{{standing.PCT}}</td>
+        <td style="width:200px;  ">{{standing.GB}}</td>
+
       </tr>
     </table>
   `,
