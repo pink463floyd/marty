ef02813fef393323853e4eba47e8fb354c12539b
Scott Larribeau <slarribeau@gmail.com>
2018-05-03 15:03:51 -0700

    small refactor: cleanup code

--- app/date/date.component.ts
+++ app/date/date.component.ts
@@ -5,12 +5,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-date',
   template: `
     <p>
-    <button (click)="updateCalcs($event)">Click me!</button>
-
        <mat-form-field>
           <input matInput [matDatepicker]="picker" [(ngModel)]="date"  (dateChange)="updateCalcs($event)" placeholder="Choose a date">
           <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
-          <mat-datepicker #picker> ngDefaultControl [(ngModel)]="date" (selectedChanged)="updateCalcs($event)" </mat-datepicker>
+          <mat-datepicker #picker>  </mat-datepicker>
       </mat-form-field> 
       {{date}}   
     </p>
