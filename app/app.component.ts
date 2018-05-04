b69838c8b49aa58f769e07fa5f26d5fae2107863
Scott Larribeau <slarribeau@gmail.com>
2018-05-04 10:41:07 -0700

    More refactoring on constructors

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,40 +7,36 @@ import { RouterModule, Routes, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title : String;
-  date2 : Date;
   filterDate : any;
+
+  constructor(private router: Router) {
+    console.log("App Component: Constructor")
+    this.title = 'app';
+
+    let tmpDate = new Date();
+    tmpDate.setDate(tmpDate.getDate()-1);
+    this.filterDate = tmpDate.getFullYear() + "-";
+    this.filterDate += tmpDate.getMonth()+1 + "-";
+    this.filterDate += tmpDate.getDate();
+  }
 
   divisions = [
     {"menu":"AL-East", "league":"AL", "division":"EAST"},
     {"menu":"AL-Central", "league":"AL", "division":"CENT"},
     {"menu":"AL-West", "league":"AL", "division":"WEST"},
   ];
+
   onSelect(division)  {
     console.log("App Component: onSelect")
     console.log(division.menu)
     console.log(this.filterDate)
-    let date2: Date = new Date();
-    date2.setDate(date2.getDate()-1);
-    let dateString = date2.getFullYear() + "-";
-    dateString += date2.getMonth()+1 + "-";
-    dateString += date2.getDate();
-
     this.router.navigate(["/subscriptions",division.menu,this.filterDate])
   }
 
   ngOnInit() {
     console.log("App Component: ngOnInit")
-  }
-
-  constructor(private router: Router) {
-    console.log("App Component: Constructor")
-    this.date2 = new Date();
-    this.date2.setDate(this.date2.getDate()-1);
-    this.filterDate = this.date2.getFullYear() + "-";
-    this.filterDate += this.date2.getMonth()+1 + "-";
-    this.filterDate += this.date2.getDate();
-    this.title = 'app';
   }
 }
