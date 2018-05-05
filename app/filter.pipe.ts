ee7d27ce3fedbd6a2e26c7f6fb71f5e180ff43c6
Scott Larribeau <slarribeau@gmail.com>
2018-05-05 15:00:30 -0700

    filter is now functioning -- we have moved beyond limping

--- app/filter.pipe.ts
+++ app/filter.pipe.ts
@@ -5,12 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
   transform(items: any[], searchText: string): any[] {
       console.log("in filter!")
-      return items;
+      console.log(searchText)
+      console.log(items.length)
+      console.log(items[0].Date)
     if(!items) return [];
     if(!searchText) return items;
-    searchText = searchText.toLowerCase();
     return items.filter( it => {
-      return it.toLowerCase().includes(searchText);
+      //return it.toLowerCase().includes(searchText);
+      console.log(it)
+      if (it.Date == searchText) {
+          return true;
+      } else {
+          return false;
+      }
     });
    }
 }
