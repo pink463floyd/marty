6b845123019e135ebd2fd06daa69a9dad7e15f2a
Scott Larribeau <slarribeau@gmail.com>
2018-06-13 13:56:48 -0700

    use strings for setting dates in tests instead of numerical params

--- app/date-utilities.service.spec.ts
+++ app/date-utilities.service.spec.ts
@@ -17,35 +17,47 @@ describe('DateUtilitiesService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should return yesterdays date from todays date', () => {
+  it('should return yesterdays date from todays date NOTE: this needs to be refactored every day :-(', () => {
      let ret: String;
      ret = dateUtil.getYesterdayString();
      console.log(ret);
      expect(ret).toEqual("2018-6-12");
   });
 
-  it('should return yesterdays date from 2019-01-01', () => {
-    testDate = new Date(2019,0,1);
+  it('should return yesterdays date from future 2019-01-01', () => {
+    testDate = new Date("January 1, 2019");
     ret = dateUtil.getPrevDayString(testDate);
     expect(ret).toEqual("2018-12-31");
   });
-/*
-  it('should return yesterdays date from 2011-10-30', () => {
-    testDate = new Date(2011,10,30);
+
+  it('should return yesterdays date from past date 2011-10-31', () => {
+    testDate = new Date("October 31, 2011");
+    ret = dateUtil.getPrevDayString(testDate);
+    expect(ret).toEqual("2011-10-30");
+  });
+
+  it('should return yesterdays date from past date 2011-10-30', () => {
+    testDate = new Date("October 30, 2011");
     ret = dateUtil.getPrevDayString(testDate);
     expect(ret).toEqual("2011-10-29");
   });
 
-  it('should return yesterdays date from 2011-10-31', () => {
-    testDate = new Date(2011,10,30);
+  it('should return yesterdays date from leap year 2020-02-29', () => {
+    testDate = new Date("February 29, 2020");
+
     ret = dateUtil.getPrevDayString(testDate);
-    expect(ret).toEqual("2011-10-29");
+    expect(ret).toEqual("2020-2-28");
   });
 
-  it('should return yesterdays date from 2011-10-30', () => {
-    testDate = new Date(2011,10,30);
+  it('should return yesterdays date from leap year 2020-03-01', () => {
+    testDate = new Date('March 1, 2020 03:24:00');
+
     ret = dateUtil.getPrevDayString(testDate);
-    expect(ret).toEqual("2011-10-29");
+    expect(ret).toEqual("2020-2-29");
   });
-  */
+
 });
+
+function tests() {
+  let str = "";
+}
